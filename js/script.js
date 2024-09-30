a7cc7dcd5cf3950a3aaf8036d8a180cc9fd0a142
M_Αmin Jafarnejad <mohammadamindf@gmail.com>
2024-09-30 11:28:43 +0330

    Clean Code's

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-"use strict"
+"use strict";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const body = document.body;
-    const toggleBtn = document.getElementById('toggleBtn');
-    const currentTheme = localStorage.getItem('theme') || 'a-mode';
-    body.classList.add(currentTheme);
-    toggleBtn.textContent = currentTheme === 'a-mode' ? '🍓' : '🐳';
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.body;
+  const toggleBtn = document.getElementById("toggleBtn");
+  const currentTheme = localStorage.getItem("theme") || "a-mode";
+  body.classList.add(currentTheme);
+  toggleBtn.textContent = currentTheme === "a-mode" ? "🍓" : "🐳";
 
-    toggleBtn.addEventListener('click', () => {
-        if (body.classList.contains('a-mode')) {
-            body.classList.replace('a-mode', 'm-mode');
-            toggleBtn.textContent = '🐳';
-            localStorage.setItem('theme', 'm-mode');
-        } else {
-            body.classList.replace('m-mode', 'a-mode');
-            toggleBtn.textContent = '🍓';
-            localStorage.setItem('theme', 'a-mode');
-        }
-    });
+  toggleBtn.addEventListener("click", () => {
+    if (body.classList.contains("a-mode")) {
+      body.classList.replace("a-mode", "m-mode");
+      toggleBtn.textContent = "🐳";
+      localStorage.setItem("theme", "m-mode");
+    } else {
+      body.classList.replace("m-mode", "a-mode");
+      toggleBtn.textContent = "🍓";
+      localStorage.setItem("theme", "a-mode");
+    }
+  });
 });
